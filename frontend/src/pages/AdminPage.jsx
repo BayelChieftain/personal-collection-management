e72ca6cef589a100a2a9135c82d81ada3aa66454
BayelChieftain <marvel.vovo@gmail.com>
2024-03-19 18:27:53 +0600

    adding the ability to block and delete a user

--- frontend/src/pages/AdminPage.jsx
+++ frontend/src/pages/AdminPage.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router-dom';
 
 const AdminPage = () => {
   const [users, setUsers] = useState([]);
-  const {isAuth} = useAuth();
+  const {isAuth, user} = useAuth();
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -34,12 +34,24 @@ const AdminPage = () => {
     }
   };
 
+  const deleteUser = async (userId) => {
+    try {
+      console.log(userId)
+      await $api.delete(`/deleteUser/${userId}`);
+      fetchUsers();
+    } catch (error) {
+      console.error('Error deleting user:', error);
+    }
+  };
+
   return isAuth ? (
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-2xl font-bold mb-4">Admin Page</h1>
+      <p className="font-Poppins text-rose-800 text-sm">Current admin: {users[0] ? user.username : null}</p>
       <table className="table-auto w-full">
         <thead>
           <tr>
+          <th className="px-2 py-1">icon</th>
             <th className="px-4 py-2">Username</th>
             <th className="px-4 py-2">Email</th>
             <th className="px-4 py-2">Role</th>
@@ -49,6 +61,7 @@ const AdminPage = () => {
         <tbody>
           {users.map((user) => (
             <tr key={user._id}>
+              <td className="border px-2 py-1"><img src={user.profilePicture} alt={`Profile icon of ${user.name}`} className="h-10 w-10 object-cover rounded-full" /></td>
               <td className="border px-4 py-2">{user.username}</td>
               <td className="border px-4 py-2">{user.email}</td>
               <td className="border px-4 py-2">{user.role}</td>
@@ -61,7 +74,13 @@ const AdminPage = () => {
                 >
                   <option value="user">User</option>
                   <option value="admin">Admin</option>
+                  <option value="blocked">Blocked</option>
                 </select>
+                <button  
+                  className="ml-2 px-2 py-1 rounded bg-red-500 text-white" 
+                  onClick={() => deleteUser(user._id)}> 
+                  Delete
+                </button>
               </td>
             </tr>
           ))}

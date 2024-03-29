fca4b455193df3a50f928438bb4055e9f9ab37c8
Bayel <114753679+BayelChieftain@users.noreply.github.com>
2024-03-29 12:50:36 +0600

    Update tokenService.js

--- backend/service/tokenService.js
+++ backend/service/tokenService.js
@@ -14,7 +14,7 @@ class TokenService {
 
     validateAccessToken(token) {
         try {
-            const userData = jwt.verify(token, JWT_ACCESS_SECRET);
+            const userData = jwt.verify(token, process.env.JWT_ACCESS_SECRET);
             return userData;
         } catch (e) {
             return null;
@@ -23,7 +23,7 @@ class TokenService {
 
     validateRefreshToken(token) {
         try {
-            const userData = jwt.verify(token, JWT_REFRESH_SECRET);
+            const userData = jwt.verify(token,  process.env.JWT_REFRESH_SECRET);
             return userData;
         } catch (e) {
             return null;
